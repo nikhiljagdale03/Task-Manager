c46712b6e220ce7f1eb19b5f961c0440daff2737
Nikhil <nikhiljagdale09@gmail.com>
2018-12-02 12:36:14 +0530

    Done!

--- app/src/main/java/com/example/sith3/myapplication/BaseActivity.java
+++ app/src/main/java/com/example/sith3/myapplication/BaseActivity.java
@@ -1,19 +1,20 @@
 package com.example.sith3.myapplication;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.sith3.myapplication.util.PreferenceHelper;
 
 public abstract class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "BaseActivity";
 
     protected PreferenceHelper  mPreferenceHelper;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"In Base Activity");
         mPreferenceHelper = PreferenceHelper.getInstance(this);
     }
 
--- app/src/main/java/com/example/sith3/myapplication/launcher/view/LauncherActivity.java
+++ app/src/main/java/com/example/sith3/myapplication/launcher/view/LauncherActivity.java
@@ -2,6 +2,7 @@ package com.example.sith3.myapplication.launcher.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.sith3.myapplication.BaseActivity;
 import com.example.sith3.myapplication.R;
@@ -10,10 +11,12 @@ import com.example.sith3.myapplication.util.PreferenceHelper;
 public class LauncherActivity extends BaseActivity {
 
     public static boolean isLogin;
+    private static final String TAG = "LauncherActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
+        setContentView(R.layout.activity_launcher);
+        Log.d(TAG,"In Launcher Activity");
         isLogin = mPreferenceHelper.getBoolean(PreferenceHelper.IS_lOGIN,false);
         if (isLogin) {
             // navigate to Home Screen
